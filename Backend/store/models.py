fa1efd63437f14e2d424529f42558a5c206d7e69
nicolas <nicoanear@gmail.com>
2024-12-04 10:35:24 -0500

    Cambio en el modelo nuevo campo añadido

--- Backend/store/models.py
+++ Backend/store/models.py
@@ -10,6 +10,7 @@ class Category(models.Model):
 # Modelo productos
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    image = models.FileField()
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=3)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
