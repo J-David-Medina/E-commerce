6ed73fcfb1df92e29bb80c0e14552c98944662ae
Juan David Medina <juandavidmedina398@gmail.com>
2024-12-04 10:32:31 -0500

    Implementado apis

--- Backend/store/urls.py
+++ Backend/store/urls.py
@@ -3,15 +3,15 @@ from rest_framework import routers
 from store import views
 
 router = routers.DefaultRouter()
-router.register(r'', views.CategoryView, 'category')
-router.register(r'', views.ProductView, 'products')
-router.register(r'', views.RoleView, 'roles')
-router.register(r'', views.UserView, 'users')
-router.register(r'', views.StateView, 'states')
-router.register(r'', views.OrderView, 'orders')
-router.register(r'', views.OrderDetailView, 'orderdetails')
+router.register(r'categories', views.CategoryView, 'category')
+router.register(r'products', views.ProductView, 'products')
+router.register(r'roles', views.RoleView, 'roles')
+router.register(r'users', views.UserView, 'users')
+router.register(r'states', views.StateView, 'states')
+router.register(r'orders', views.OrderView, 'orders')
+router.register(r'order-details', views.OrderDetailView, 'orderdetails')
 
 
 urlpatterns = [
-    path("api/v1", include(router.urls))
+    path("api/v1/", include(router.urls))
 ]
